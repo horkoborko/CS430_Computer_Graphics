f41290dbe8b85cd3b7ef15bef3033a763d0d47ad
horkoborko <47957160+horkoborko@users.noreply.github.com>
2022-10-13 08:46:00 -0700

    Update raycast.c
    Worked on main method

--- Project3/raycast.c
+++ Project3/raycast.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Structures 
-struct plane 
+// Structures
+struct plane
 {
    int colorArray[3];
    float position[3];
    int normal[3];
-   bool closestObject; 
+   bool closestObject;
 
 } plane;
 
@@ -16,32 +16,35 @@ struct sphere
 {
    int colorArray[3];
    float position[3];
-   int radius; 
+   int radius;
    bool closestObject;
 
-} sphere; 
+} sphere;
 
 struct camera
 {
-   int width; 
-   int height; 
+   int width;
+   int height;
    int position[3];
 } camera;
 
 // Function Prototypes
 void raycast(int width, int height, char *inputFile, char *outputFile);
 
-// Main Function 
-int main()
+// Main Function
+int main(int argc, char *argv[])
 {
-   // if the program was called with 5 total arguments 
+   int size = sizeof prices / sizeof prices[0];
 
-      // raycast the input into output 
-         // function: raycast
-	
-   // otherwise, improper format 
-
-      // print improper format
+   // if the program was called with 5 total arguments
+   if(size <= 5)
+   {
+     raycast(argv[1], argv[2], argv[3], argv[4]);
+   }
+   else
+   {
+     printf("ERROR: Format was improper; was expecting five arguments, bur received %d", size);
+   }
 
    return 0;
 }
@@ -49,90 +52,90 @@ int main()
 // Function Implementation
 void raycast(int width, int height, char *inputFile, char *outputFile)
 {
-   // initalize variables 
+   // initalize variables
 
       // create an array of objects
 
-      // create a temp object for each object 
+      // create a temp object for each object
 
-   // read in data 
-    
-      // open the file 
-         // function: fopen 
+   // read in data
 
-      // while not at end of file 
+      // open the file
+         // function: fopen
+
+      // while not at end of file
 
 	 // reset flags
 
-         // read in the string 
+         // read in the string
 
-         // if we are a camera 
+         // if we are a camera
 
-            // set a camera flag 
-	    
+            // set a camera flag
+
          // if we are a sphere
 
-            // set a sphere flag 
-	    
-         // if we are a plane 
+            // set a sphere flag
 
-            // set a plane flag 
-	        
-         // if we are not at the end of the file 
+         // if we are a plane
 
-            // if we are a camera 
+            // set a plane flag
 
-               // read in width 
+         // if we are not at the end of the file
 
-	    // otherwise, if sphere or plane 
+            // if we are a camera
 
-               // read in color 
+               // read in width
 
-         // if we are not at the end of the file 
+	    // otherwise, if sphere or plane
 
-            // if we are a camera 
+               // read in color
 
-	       // read in height 
+         // if we are not at the end of the file
+
+            // if we are a camera
+
+	       // read in height
 
 	    // otherwise, if plane or sphere
-	       
+
 	       // read in position
 
-	// if we are not at the end of the file 
-	
-	    // if we are a plane 
+	// if we are not at the end of the file
 
-	       // read in the normal vector 
+	    // if we are a plane
+
+	       // read in the normal vector
 
             // otherwise, if we are a sphere
 
-	       // read in the radius 
+	       // read in the radius
 
-            // otherwise, camera or other object, do nothing. 
+            // otherwise, camera or other object, do nothing.
 
 	// if we are a camera, a plane, or a sphere
-	
+
 	   // store the object in the object array
 
-   // for every x on the image 
+   // for every x on the image
 
-      // for every y on the image 
+      // for every y on the image
 
-         // for every object that we have 
+         // for every object that we have
 
-            // calculate the t of each object 
+            // calculate the t of each object
 
-            // if the t is the lowest t so far 
+            // if the t is the lowest t so far
 
-               // rebind lowest t to new t 
+               // rebind lowest t to new t
 
-               // set the new closest object 
+               // set the new closest object
 
-           // reset t 
+           // reset t
 
-           // put the color on the closest object into the end pixmap 
+           // put the color on the closest object into the end pixmap
 
-           // reset closest object to first object 
+           // reset closest object to first object
 
    // write the pixmap to the output file in p3
 
